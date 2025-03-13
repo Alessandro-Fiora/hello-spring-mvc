b99ddd185e89d406fd18691bc3d1f7cd9421fcef
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-03-13 17:33:28 +0100

    feat: add dynamic saluta page

--- src/main/java/org/lessons/java/hello_spring_mvc/controllers/HomeController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package org.lessons.java.hello_spring_mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -13,6 +15,14 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/saluta")
+    public String saluta(Model model, @RequestParam(name = "name") String name) {
+
+        model.addAttribute("name", name);
+
+        return "saluta";
     }
     
 
